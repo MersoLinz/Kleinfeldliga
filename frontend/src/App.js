757eff1b007a81f4612c9973804861ea1d4de884
Merso <mbesic@gmx.at>
2025-04-22 21:09:00 +0200

    Navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ResponsiveAppBar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <ResponsiveAppBar /> <p>Hallo</p>
+    </>
+  );
+}
+
+export default App;
